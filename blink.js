6a6a10d31fb447a1e51899c6f95882dfb11546c2
MarlenaFlueh <m.fluehh@gmail.com>
2019-05-02 19:49:05 +0100

    add db connection

--- blink.js
+++ blink.js
@@ -1,11 +1,11 @@
 const Gpio = require('onoff').Gpio;
-const LED = new Gpio(4, 'out'); // gpio 4 as out
+const LED = new Gpio(3, 'out'); // gpio 3 as out
 
-LED.writeSync(1); // making the gpio 4 on. Will turn LED on
+LED.writeSync(1); // making the gpio 3 on. Will turn LED on
 
 
 function switchOff() {
-    LED.writeSync(0); // making the gpio 4 off. Will turn LED off
+    LED.writeSync(0); // making the gpio 3 off. Will turn LED off
     LED.unexport(); // Unexport GPIO to free resources
 }
 
